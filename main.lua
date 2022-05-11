cdd2fb35cf31eb26555225e89a18036751ae8d20
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-05-11 09:59:43 +0200

    Add PajFindFromVendor and PajBuyFromVendor functions

--- main.lua
+++ main.lua
@@ -15,6 +15,42 @@ function PajFindItem(item_name)
     return items
 end
 
+function PajFindFromVendor(item_name)
+    local numMerchantItems = GetMerchantNumItems()
+
+    if numMerchantItems == 0 then
+        return 0
+    end
+
+    for i = 1, numMerchantItems do
+        local name, _, _, _, _, _, _ = GetMerchantItemInfo(i)
+        if name == item_name then
+            return i
+        end
+    end
+
+    return 0
+end
+
+function PajBuyFromVendor(item_name, quantity, stack_size)
+    local itemIndex = PajFindFromVendor(item_name)
+    if itemIndex == 0 then
+        return
+    end
+
+    if quantity == nil then
+        quantity = 1
+    end
+
+    if stack_size == nil then
+        stack_size = 1
+    end
+
+    for i = 1, quantity do
+        BuyMerchantItem(itemIndex, stack_size)
+    end
+end
+
 function PajTradeItem(item_name)
     local items = PajFindItem(item_name)
     local i = 1
